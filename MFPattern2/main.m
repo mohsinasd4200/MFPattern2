9e995524fea68fd76c9f9c0349ec5c97469a3431
Mohsin Farooqui <mohsin.farooqui4@gmail.com>
2016-10-24 12:55:45 +0530

    feeling excited

--- MFPattern2/main.m
+++ MFPattern2/main.m
@@ -6,12 +6,35 @@
 //  Copyright © 2016 Mohsin Farooqui. All rights reserved.
 //
 
+void pattern1();
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+        
+        
+        
+        
+        
+            pattern1();
+        
+        
+        
+            }
     return 0;
+}
+
+void pattern1()
+{
+    int i, j;
+    
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d\t", i);
+        }
+        printf("\n");
+    }
+    
 }
